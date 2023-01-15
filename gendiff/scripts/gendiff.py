132a83cb17dde6bdf37ddc8e197ddaae6d559236
AlexStrrr <alex.hrtnv@gmail.com>
2023-01-15 19:03:14 +0300

    add scripts/gendiff.py

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Generate diff')
+    parser.add_argument('first_file', type=str)
+    parser.add_argument('second_file', type=str)
+    parser.add_argument('-f', '--format', default="stylish",
+                        help='set format of output (default: "stylish") or Plain or JSON')
+    args = parser.parse_args()
+    return args.first_file, args.second_file, args.format
+
+
+if __name__ == '__main__':
+    main()
