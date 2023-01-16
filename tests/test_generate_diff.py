193c66ac97896d349619eeef40bb30af875ba099
AlexStrrr <alex.hrtnv@gmail.com>
2023-01-16 19:30:17 +0300

    add tests and updated gendiff/scripts/gendiff.py

---
+++ tests/test_generate_diff.py
@@ -0,0 +1,23 @@
+from gendiff.gendiff import generate_diff
+
+
+test_file1 = {
+  "host": "hexlet.io",
+  "timeout": 50,
+  "proxy": "123.234.53.22",
+  "follow": false
+}
+
+
+test_file2 = {
+  "timeout": 20,
+  "verbose": true,
+  "host": "hexlet.io"
+}
+
+#
+# def test_gen_diff():
+#     result = generate_diff(test_file1, test_file2)
+#     assert result ==
+
+
