0c93e6e30dffd177b4f0ee5fc87024388a492829
AlexStrrr <alex.hrtnv@gmail.com>
2023-01-21 15:20:28 +0300

    upd test_generate_diff.py

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,8 +1,18 @@
 from gendiff.gendiff import generate_diff
 
 
-def test_gen_diff():
+def test_gen_diff_json():
     result = generate_diff("tests/fixtures/file1.json", "tests/fixtures/file2.json")
+    assert result == """- follow False
+  host hexlet.io
+- proxy 123.234.53.22
+- timeout 50
++ timeout 20
++ verbose True\n"""
+
+
+def test_gen_diff_yaml():
+    result = generate_diff("tests/fixtures/filepath1.yml", "tests/fixtures/filepath2.yml")
     assert result == """- follow False
   host hexlet.io
 - proxy 123.234.53.22
