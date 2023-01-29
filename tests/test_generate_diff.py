8bd2dc17f4d5484b3a2b5a002e78351f7d547c14
AlexStrrr <alex.hrtnv@gmail.com>
2023-01-29 16:21:09 +0300

    add test for make_diff

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,21 +1,17 @@
 from gendiff.gendiff import generate_diff
+from gendiff.gendiff import make_diff
 
 
 def test_gen_diff_json():
     result = generate_diff("tests/fixtures/file1.json", "tests/fixtures/file2.json")
-    assert result == """- follow False
-  host hexlet.io
-- proxy 123.234.53.22
-- timeout 50
-+ timeout 20
-+ verbose True\n"""
+    assert result == "tests/fixtures/results/flat.txt"
 
 
 def test_gen_diff_yaml():
     result = generate_diff("tests/fixtures/file1.yml", "tests/fixtures/file2.yml")
-    assert result == """- follow False
-  host hexlet.io
-- proxy 123.234.53.22
-- timeout 50
-+ timeout 20
-+ verbose True\n"""
+    assert result == "tests/fixtures/results/flat.txt"
+
+
+def test_make_diff():
+    result = make_diff("tests/fixtures/file3.json", "tests/fixtures/file4.json")
+    assert result == "tests/fixtures/results/nested.txt"
