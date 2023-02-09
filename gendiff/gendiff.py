abb047c433b36ea2f4b8555a68a5a5d9ded79761
AlexStrrr <alex.hrtnv@gmail.com>
2023-02-09 14:59:54 +0300

    upd gendiff.py

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -2,45 +2,45 @@ from parser import parser
 import pprint
 
 
-# def gen_diff_files(file1, file2):
-#     result = ''
-#     keys = list(file1.keys() | file2.keys())
-#     keys.sort()
-#     for k in keys:
-#         if k in file1 and k in file2:
-#             if file1[k] == file2[k]:
-#                 result += f"  {k} {file1[k]}\n"
-#                 continue
-#         if k in file1:
-#             result += f"- {k} {file1[k]}\n"
-#         if k in file2:
-#             result += f"+ {k} {file2[k]}\n"
-#     return result
+ADDED = 'added'
+REMOVED = 'removed'
+UNCHANGED = 'unchanged'
+UPDATED = 'updated'
+NESTED = 'nested'
+INNER_UPDATED = 'inner_updated'
 
 
 def make_diff(file1, file2):
+
     all_keys = list(file1.keys() | file2.keys())
     all_keys.sort()
     diff_dict = {}
+
     for key in all_keys:
+
         if key in file1.keys() and key in file2.keys():
             if file1[key] != file2[key]:
                 if isinstance(file1[key], dict) and isinstance(file2[key], dict):
-                    diff_dict[key] = {'type': 'changed',
+                    diff_dict[key] = {'type': INNER_UPDATED,
                                       'value': make_diff(file1[key], file2[key])}
+
                 else:
-                    diff_dict[key] = {'type': 'changed',
+                    diff_dict[key] = {'type': UPDATED,
                                       'old_value': file1[key],
                                       'new_value': file2[key]}
+
             else:
-                diff_dict[key] = {'type': 'unchanged',
+                diff_dict[key] = {'type': UNCHANGED,
                                   'value': file1[key]}
+
         elif key in file1:
-            diff_dict[key] = {'type': 'deleted',
+            diff_dict[key] = {'type': REMOVED,
                               'value': file1[key]}
+
         elif key in file2:
-            diff_dict[key] = {'type': 'added',
+            diff_dict[key] = {'type': ADDED,
                               'value': file2[key]}
+
     pprint.pprint(diff_dict)
     return diff_dict
 
