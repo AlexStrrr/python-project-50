90d60be11b6387553dcfd0211085fc31b8e25485
AlexStrrr <alex.hrtnv@gmail.com>
2023-02-11 12:45:55 +0300

    upd gendiff.py

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,5 +1,4 @@
 from parser import parser
-from formatters.plain import plain_formatter
 
 
 ADDED = 'added'
@@ -47,6 +46,5 @@ def make_diff(file1, file2):
 def generate_diff(file1, file2, formatter='stylish'):
     f1 = parser(file1)
     f2 = parser(file2)
-    format_style = plain_formatter(formatter)
     diff_dict = make_diff(f1, f2)
     return format_style(diff_dict)
