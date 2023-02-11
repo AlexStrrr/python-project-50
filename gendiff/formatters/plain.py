ad688e0fc838b065ac1bef5ee8b4070c84200852
AlexStrrr <alex.hrtnv@gmail.com>
2023-02-11 12:42:52 +0300

    upd plain formatter

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -1,132 +1,84 @@
-# dictt = {'common': {'type': 'inner_updated',
-#             'value': {'follow': {'type': 'added', 'value': False},
-#                       'setting1': {'type': 'unchanged', 'value': 'Value 1'},
-#                       'setting2': {'type': 'removed', 'value': 200},
-#                       'setting3': {'new_value': None,
-#                                    'old_value': True,
-#                                    'type': 'updated'},
-#                       'setting4': {'type': 'added', 'value': 'blah blah'},
-#                       'setting5': {'type': 'added',
-#                                    'value': {'key5': 'value5'}},
-#                       'setting6': {'type': 'inner_updated',
-#                                    'value': {'doge': {'type': 'inner_updated',
-#                                                       'value': {'wow': {'new_value': 'so '
-#                                                                                      'much',
-#                                                                         'old_value': '',
-#                                                                         'type': 'updated'}}},
-#                                              'key': {'type': 'unchanged',
-#                                                      'value': 'value'},
-#                                              'ops': {'type': 'added',
-#                                                      'value': 'vops'}}}}},
-#  'group1': {'type': 'inner_updated',
-#             'value': {'baz': {'new_value': 'bars',
-#                               'old_value': 'bas',
-#                               'type': 'updated'},
-#                       'foo': {'type': 'unchanged', 'value': 'bar'},
-#                       'nest': {'new_value': 'str',
-#                                'old_value': {'key': 'value'},
-#                                'type': 'updated'}}},
-#  'group2': {'type': 'removed', 'value': {'abc': 12345, 'deep': {'id': 45}}},
-#  'group3': {'type': 'added',
-#             'value': {'deep': {'id': {'number': 45}}, 'fee': 100500}}}
-#
-# import itertools
-# import json
-#
-# ADDED = 'added'
-# REMOVED = 'removed'
-# UNCHANGED = 'unchanged'
-# UPDATED = 'updated'
-# NESTED = 'nested'
-# INNER_UPDATED = 'inner_updated'
-# COMPLEX_VALUE = 'complex_value'
-#
-#
-# ADDED_TEMPL = "Property '{}' was added with value: {}"
-# REMOVED_TEMPL = "Property '{}' was removed"
-# UPDATED_TEMPL = "Property '{}' was updated. From {} to {}"
-# PATH = "{}.{}"
-#
-#
-# # def recurse(diffdict, path=''):
-# #     for key in diffdict:
-# #         for rv in recurse(diffdict[key], path + key):
-# #             return rv
-# #     else:
-# #         return path
-#
-#
-#
-# #print(recurse(dictt))
-#
-#
-# # def formatter_plain(diffdict: dict, source='') -> str:
-# #     result = ''
-# #     for key in diffdict:
-# #         path = PATH.format(source, key) if source else key
-# #         if diffdict[key]['type'] == ADDED:
-# #             result += f"Property {path} was added with value: {diffdict[key]['value']}\n"
-# #         elif diffdict[key]['type'] == REMOVED:
-# #             result += f"Property {path} was removed\n"
-# #         elif diffdict[key]['type'] == UPDATED:
-# #             result += f"Property {path} was updated. From {diffdict[key]['old_value']}" \
-# #                       f" to {diffdict[key]['new_value']}\n"
-# #         # elif diffdict[key]['type'] == INNER_UPDATED:
-# #         #     if diffdict[key]['value'] and isinstance(diffdict[key]['value'], dict):
-# #         #         result += f"{formatter_plain(diffdict[key]['value'], source)}"
-# #     return result
+dictt = {'common': {'type': 'inner_updated',
+            'value': {'follow': {'type': 'added', 'value': False},
+                      'setting1': {'type': 'unchanged', 'value': 'Value 1'},
+                      'setting2': {'type': 'removed', 'value': 200},
+                      'setting3': {'new_value': None,
+                                   'old_value': True,
+                                   'type': 'updated'},
+                      'setting4': {'type': 'added', 'value': 'blah blah'},
+                      'setting5': {'type': 'added',
+                                   'value': {'key5': 'value5'}},
+                      'setting6': {'type': 'inner_updated',
+                                   'value': {'doge': {'type': 'inner_updated',
+                                                      'value': {'wow': {'new_value': 'so '
+                                                                                     'much',
+                                                                        'old_value': '',
+                                                                        'type': 'updated'}}},
+                                             'key': {'type': 'unchanged',
+                                                     'value': 'value'},
+                                             'ops': {'type': 'added',
+                                                     'value': 'vops'}}}}},
+ 'group1': {'type': 'inner_updated',
+            'value': {'baz': {'new_value': 'bars',
+                              'old_value': 'bas',
+                              'type': 'updated'},
+                      'foo': {'type': 'unchanged', 'value': 'bar'},
+                      'nest': {'new_value': 'str',
+                               'old_value': {'key': 'value'},
+                               'type': 'updated'}}},
+ 'group2': {'type': 'removed', 'value': {'abc': 12345, 'deep': {'id': 45}}},
+ 'group3': {'type': 'added',
+            'value': {'deep': {'id': {'number': 45}}, 'fee': 100500}}}
 
 
 import itertools
-import json
 
 
-def get_value(val):
-
-    if isinstance(val, dict):
-        output_val = '[complex value]'
-    elif isinstance(val, bool) or val is None:
-        output_val = json.dumps(val)
-    elif isinstance(val, int) or isinstance(val, float):
-        output_val = str(val)
-    elif isinstance(val, str):
-        output_val = f"'{str(val)}'"
-    return output_val
+ADDED = 'added'
+REMOVED = 'removed'
+UNCHANGED = 'unchanged'
+UPDATED = 'updated'
+NESTED = 'nested'
+INNER_UPDATED = 'inner_updated'
+COMPLEX_VALUE = 'complex_value'
 
 
-def get_dict(diffdict):
-    return dict(filter(lambda item: item[1]['type'] != 'unchanged', diffdict.items()))
+def get_val(value):
+    if isinstance(value, dict):
+        return "[complex value]"
+    elif isinstance(value, bool):
+        return str.lower(str(value))
+    elif value is None:
+        return 'null'
+    return "'" + str(value) + "'"
 
 
-def plain_formatter(diffdict):
+def plain_format(diffdict: dict, source='') -> str:
+    result = ''
     path = []
+    for key in diffdict:
+        # path = itertools.chain(path, str(key))
+        # path = list(path)
+        # path = ''.join(path)
+        path.append(key)
+        if diffdict[key]['type'] == UNCHANGED:
+            continue
+        elif diffdict[key]['type'] == ADDED:
+            value = get_val(diffdict[key]['value'])
+            result += f"Property '{path}' was added with value: {value}\n"
+        elif diffdict[key]['type'] == REMOVED:
+            result += f"Property '{path}' was removed\n"
+        elif diffdict[key]['type'] == UPDATED:
+            old_value = get_val(diffdict[key]['old_value'])
+            new_value = get_val(diffdict[key]['new_value'])
+            result += f"Property '{path}' was updated. From {old_value}" \
+                      f" to {new_value}\n"
+        elif diffdict[key]['type'] == INNER_UPDATED:
+            if diffdict[key]['value'] and isinstance(diffdict[key]['value'], dict):
+                path.append(key)
+                result += f"{plain_format(diffdict[key]['value'], source)}"
+    return result
 
-    def make_lines(item, path):
-        name, attributes = item
-        status = attributes['type']
-        new_line = ''
-        path_record = list(itertools.chain(path, [str(name)]))
 
-        if status == 'internal_change':
-            children = attributes['value']
-            new_line += '\n'.join(list(map(
-                                 lambda item: make_lines(item, path_record),
-                                 get_dict(children).items())))
+print(plain_format(dictt))
 
-        elif status == 'added':
-            val = attributes['value']
-            new_line += "Property '{}' was added with value: {}".format(
-                '.'.join(path_record), get_value(val))
-
-        elif status == 'deleted':
-            new_line += "Property '{}' was removed".format('.'.join(path_record))
-
-        elif status == 'changed_value':
-            del_val, add_val = attributes['value']
-            new_line += "Property '{}' was updated. From {} to {}".format(
-                '.'.join(path_record), get_value(del_val), get_value(add_val))
-
-        return new_line
-
-    return '\n'.join(list(map(lambda item: make_lines(item, path),
-                              get_dict(diffdict).items())))
