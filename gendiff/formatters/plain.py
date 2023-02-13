829f5e9685147c6a10da5da06a01eaf9ac27f09a
AlexStrrr <alex.hrtnv@gmail.com>
2023-02-14 01:07:06 +0300

    upd plain.py

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -1,35 +1,45 @@
-from gendiff.run.gendiff import ADDED, REMOVED, UNCHANGED, UPDATED, INNER_UPDATED
+from gendiff.formatters.stylish import bool_null
+from tests.fixtures.types import ADDED, REMOVED, UNCHANGED, UPDATED, INNER_UPDATED
 
 
 def get_val(value):
-    if isinstance(value, dict):
+
+    if isinstance(value, bool) or value is None:
+        return bool_null(value, False)
+
+    elif isinstance(value, dict):
         return "[complex value]"
-    elif isinstance(value, bool):
-        return str.lower(str(value))
-    elif value is None:
-        return 'null'
+
     return "'" + str(value) + "'"
 
 
 def plain_format(diff_dict: dict, path=[]) -> str:
     result = ''
+
     for key in diff_dict:
         path.append(key)
+
         if diff_dict[key]['type'] == UNCHANGED:
             path.pop()
             continue
+
         elif diff_dict[key]['type'] == ADDED:
             value = get_val(diff_dict[key]['value'])
             result += f"Property '{'.'.join(path)}' was added with value: {value}\n"
+
         elif diff_dict[key]['type'] == REMOVED:
             result += f"Property '{'.'.join(path)}' was removed\n"
+
         elif diff_dict[key]['type'] == UPDATED:
             old_value = get_val(diff_dict[key]['old_value'])
             new_value = get_val(diff_dict[key]['new_value'])
             result += f"Property '{'.'.join(path)}' was updated. From {old_value}" \
                       f" to {new_value}\n"
+
         elif diff_dict[key]['type'] == INNER_UPDATED:
             if diff_dict[key]['value'] and isinstance(diff_dict[key]['value'], dict):
                 result += f"{plain_format(diff_dict[key]['value'], path)}"
+
         path.pop()
+
     return result
