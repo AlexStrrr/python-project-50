4c6addd00b7ede31f3462ca26e72ba6db74b4f80
AlexStrrr <alex.hrtnv@gmail.com>
2023-03-02 23:09:39 +0300

    upd test_generate_diff.py

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -18,28 +18,27 @@ def test_generate_diff_flat(data_file1, data_file2, result_file):
     assert generate_diff(data_file1, data_file2) == result
 
 
-@pytest.mark.parametrize('data_file1, data_file2, result_file',
+@pytest.mark.parametrize('file1, file2, format, result_file',
                          [('tests/fixtures/file3.json',
                            'tests/fixtures/file4.json',
-                           'tests/fixtures/results/stylish.txt'),
-                          ('tests/fixtures/file3.yaml',
-                           'tests/fixtures/file4.yaml',
-                           'tests/fixtures/results/stylish.txt')])
-def test_generate_diff_stylish(data_file1, data_file2, result_file):
-    result = open(result_file, 'r').read()
-    assert generate_diff(data_file1, data_file2) == result
-
-
-@pytest.mark.parametrize('data_file1, data_file2, result_file',
-                         [('tests/fixtures/file3.json',
-                           'tests/fixtures/file4.json',
+                           'plain'
                            'tests/fixtures/results/plain.txt'),
                           ('tests/fixtures/file3.yaml',
                            'tests/fixtures/file4.yaml',
-                           'tests/fixtures/results/plain.txt')])
-def test_generate_diff_plain(data_file1, data_file2, result_file):
-    result = open(result_file, 'r').read()
-    assert generate_diff(data_file1, data_file2) == result
+                           'plain'
+                           'tests/fixtures/results/plain.txt'),
+                          ('tests/fixtures/file3.json',
+                           'tests/fixtures/file4.json',
+                           'stylish'
+                           'tests/fixtures/results/stylish.txt'),
+                          ('tests/fixtures/file3.yaml',
+                           'tests/fixtures/file4.yaml',
+                           'stylish'
+                           'tests/fixtures/results/stylish.txt')])
+def test_generate_diff(file1, file2, format, result_file):
+    with open(result_file) as file:
+        result = file.read()
+    assert result == generate_diff(file1, file2, format)
 
 
 def is_json(data):
