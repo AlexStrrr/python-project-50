c44a1c2bf27cd466adf4ab69199e9f5b2be5b400
AlexStrrr <alex.hrtnv@gmail.com>
2023-03-19 22:34:28 +0300

    upd gendiff.py

--- gendiff/run/gendiff.py
+++ gendiff/run/gendiff.py
@@ -10,38 +10,38 @@ NESTED = 'nested'
 INNER_UPDATED = 'inner_updated'
 
 
-def make_diff(f1, f2):
+def make_diff(file1, file2):
 
-    all_keys = list(f1.keys() | f2.keys())
+    all_keys = list(file1.keys() | file2.keys())
     all_keys.sort()
     diff_dict = {}
 
     for key in all_keys:
 
-        if key in f1.keys() and key in f2.keys():
-            if f1[key] != f2[key]:
-                if isinstance(f1[key], dict)\
-                        and isinstance(f2[key], dict):
+        if key in file1.keys() and key in file2.keys():
+            if file1[key] != file2[key]:
+                if isinstance(file1[key], dict)\
+                        and isinstance(file2[key], dict):
                     diff_dict[key] = {'type': INNER_UPDATED,
-                                      'value': make_diff(f1[key],
-                                                         f2[key])}
+                                      'value': make_diff(file1[key],
+                                                         file2[key])}
 
                 else:
                     diff_dict[key] = {'type': UPDATED,
-                                      'old_value': f1[key],
-                                      'new_value': f2[key]}
+                                      'old_value': file1[key],
+                                      'new_value': file2[key]}
 
             else:
                 diff_dict[key] = {'type': UNCHANGED,
-                                  'value': f1[key]}
+                                  'value': file1[key]}
 
-        elif key in f1:
+        elif key in file1:
             diff_dict[key] = {'type': REMOVED,
-                              'value': f1[key]}
+                              'value': file1[key]}
 
-        elif key in f2:
+        elif key in file2:
             diff_dict[key] = {'type': ADDED,
-                              'value': f2[key]}
+                              'value': file2[key]}
 
     return diff_dict
 
