6e7d8c4a8063c89d09e8e0cd2639e420aed1bc57
AlexStrrr <alex.hrtnv@gmail.com>
2023-03-25 15:11:06 +0300

    upd gendiff name

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,5 +1,5 @@
 import json
-from gendiff.generate_diff import generate_diff
+from gendiff.gendiff import generate_diff
 import pytest
 from gendiff.formatters.all_formatters import STYLISH, PLAIN, JSON
 
