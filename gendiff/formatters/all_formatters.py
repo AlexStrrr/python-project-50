8ce9a63d257bc7dfae6dcef7aee044698f8a5831
AlexStrrr <alex.hrtnv@gmail.com>
2023-03-31 10:46:58 +0300

    upd all_formatters.py

--- gendiff/formatters/all_formatters.py
+++ gendiff/formatters/all_formatters.py
@@ -1,5 +1,5 @@
 from gendiff.formatters.json_formatter import json_format
-from gendiff.formatters.stylish import stylish_format
+from gendiff.formatters.stylish import stylish_f
 from gendiff.formatters.plain import plain_format
 
 
@@ -12,7 +12,7 @@ FORMATS = (STYLISH, PLAIN, JSON)
 
 def format_change(diff_dict: dict, format_name: str):
     if format_name == STYLISH:
-        return stylish_format(diff_dict)
+        return stylish_f(diff_dict)
     elif format_name == PLAIN:
         return plain_format(diff_dict)[:-1]
     elif format_name == JSON:
